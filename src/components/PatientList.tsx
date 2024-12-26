990a9db366f06611dc8264ee3f47736d2231ddac
f3r922 <f3rna92@gmail.com>
2024-12-26 11:45:55 -0300

    fix_space

--- src/components/PatientList.tsx
+++ src/components/PatientList.tsx
@@ -13,7 +13,7 @@ export const PatientList = () => {
           <>
             <h2 className="font-black text-3xl text-center">Listado de Pacientes</h2>
             <p className="text-xl mt-5 mb-10 text-center">
-              Administra tus{''}
+              Administra tus{' '}
               <span className="text-indigo-600 font-bold">Pacientes y citas</span>
             </p>
             {
